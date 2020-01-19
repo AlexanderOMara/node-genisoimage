7eb46dd30dbcab68ad205a55783d0444301b1f8f
Alexander O'Mara <me@alexomara.com>
2020-01-19 18:14:48 -0500

    Build caching

--- scripts/build-cache.sh
+++ scripts/build-cache.sh
@@ -13,11 +13,37 @@ __self="${BASH_SOURCE[0]}"
 __dir="$(cd "$(dirname "${__self}")" > /dev/null && pwd)"
 __file="${__dir}/$(basename "${__self}")"
 
+dir_path='lib'
+file_path='lib.tar.gz'
+repo='AlexanderOMara/node-genisoimage'
+release='22960555'
+
 commit_id="$(git rev-parse HEAD)"
 build_number="$1"
 build_id="${commit_id}-${build_number}"
+file_name="$(basename "${file_path}")"
+build_id_file_name="${build_id}-${file_name}"
 
 cd "${__dir}/.."
 
-echo "TODO: Cache"
 echo "build_id: ${build_id}"
+tar -cz -f "${file_path}" "${dir_path}"
+shasum -a 256 "${file_path}"
+
+echo "Uploading..."
+
+output="$(curl -s \
+    -H "Authorization: token ${GITHUB_API_KEY}" \
+    -H "Content-Type: application/octet-stream" \
+    --data-binary "@${file_path}" \
+    "https://uploads.github.com/repos/${repo}/releases/${release}/assets?name=${build_id_file_name}"
+)"
+
+rm "${file_path}"
+
+if [[ "$output" == *'"errors":'* ]]; then
+	echo "ERROR:"
+	echo "$output"
+	exit 1
+fi
+echo "Done"
