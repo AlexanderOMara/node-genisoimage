2d3e9eb58f2890b79948e4fc5349597270ef7b91
Alexander O'Mara <me@alexomara.com>
2020-01-19 19:40:37 -0500

    Test disk image content for volume ID

--- scripts/test.sh
+++ scripts/test.sh
@@ -10,14 +10,26 @@ __file="${__dir}/$(basename "${__self}")"
 
 cd "${__dir}/.."
 
+errored=0
+
 # TODO: Not very thorough testing, could use more tests.
 rm -rf 'spec/disk.iso'
-./bin/node-genisoimage -D -V 'volume' -no-pad -r -o 'spec/disk.iso' 'spec/disk'
+vuid='9770f5913f4042ad9d30ea16f93f85e7'
+./bin/node-genisoimage -D -V "${vuid}" -no-pad -r -o 'spec/disk.iso' 'spec/disk'
 output="$(strings 'spec/disk.iso')"
-uid="$(cat spec/disk/dir/sub/uid.txt)"
-if [[ "${output}" == *"${uid}"* ]]; then
-	echo 'PASS: spec/disk.iso'
+
+if [[ "${output}" == *"${vuid}"* ]]; then
+	echo 'PASS: spec/disk.iso: Volume UID present'
 else
-	echo 'FAIL: spec/disk.iso'
-	exit 1
+	echo 'FAIL: spec/disk.iso: Volume UID missing'
+	errored=1
 fi
+
+if [[ "${output}" == *"$(cat 'spec/disk/dir/sub/uid.txt')"* ]]; then
+	echo 'PASS: spec/disk.iso: File UID present'
+else
+	echo 'FAIL: spec/disk.iso: File UID missing'
+	errored=1
+fi
+
+exit "${errored}"
